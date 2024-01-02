e0b6a757d774c6b97453c02b13e005338d445b4c
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-01-02 17:27:39 +0500

    string

---
+++ lesson_4/index.js
@@ -0,0 +1,49 @@
+/*
+String data type- bu matnli tip
+    * E’lon qilish turlari - “”; ‘’; ``;
+    * \n - new line in single and double quotes
+    * \t -tab
+    * Str.length
+    * Accesing character 
+        * str[I];
+        * Str.at(i)
+    * the last character
+        * str[str.lenght-1]
+        * Str.at(-1)
+
+*/
+
+//1 Declare qilish turlari
+// let singlequote='Hello I am single';
+// let doubleQuote="Hello I am double";
+// let bactic=`Hello I am bactic`
+// console.log(singlequote);
+// console.log(doubleQuote);
+// console.log(bactic);
+
+//2 Difference between declaration
+
+// single quote and doublequote works the same
+// let single='hi i\'m single';
+// let double="hi i'm single";
+// console.log(single);
+// console.log(double);
+
+//3 new line \n
+// let single='i\'m \n single';
+// console.log(single);
+
+//4 tab - \t
+// let tab='\t i am tab';
+// console.log(tab);
+
+//5 the preferences of bactic
+
+// let bactic =` i am 
+
+// bactic`;
+// console.log(bactic);
+
+// let name='Frank';
+
+// console.log(`hi my name is ${name}`);
