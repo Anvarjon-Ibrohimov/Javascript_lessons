c6f0d25a7c1c704da52bbd8fed2a245a7737a3d8
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-01-02 18:25:34 +0500

    str methods

--- lesson_4/index.js
+++ lesson_4/index.js
@@ -7,9 +7,30 @@ String data type- bu matnli tip
     * Accesing character 
         * str[I];
         * Str.at(i)
+    
     * the last character
+    
         * str[str.lenght-1]
         * Str.at(-1)
+    
+    * String methods
+        * IndexOf
+        * lastindexOf
+        * Search
+        * Includes
+        * Slice
+        * Splice
+        * Substring
+        * Substr
+        * Replace
+        * Split
+        * Repeat
+        * startWith
+        * endsWith
+        * toLowercase
+        * toUppercase
+        * Trim
+
 
 */
 
@@ -47,3 +68,56 @@ String data type- bu matnli tip
 // let name='Frank';
 
 // console.log(`hi my name is ${name}`);
+
+//6 Accessing characters str
+// let str='I am String element';
+//* calling first elem
+
+// console.log(str[0]);
+// console.log(str.at(0));
+// console.log(str.charAt(0));
+
+//* the number of elem str.length
+// console.log(str.length);
+
+//* calling last elem
+
+// console.log(str[str.length-1]);
+// console.log(str.at(-1));
+
+//* difference them
+// console.log(str[-2]); //undefined
+// console.log(str.at(-2)); //n
+
+/*
+ * String methods
+        * IndexOf +
+        * lastindexOf +
+        * Search +
+        * Includes +
+        * Slice
+        * Splice
+        * Substring
+        * Substr
+        * Replace
+        * Split
+        * Repeat
+        * startWith +
+        * endsWith +
+        * toLowercase
+        * toUppercase
+        * Trim
+*/
+
+    //1 indexof,lastindexof
+    let str='Hi i am str';
+    
+    // console.log(str.indexOf('i'));
+    // console.log(str.lastIndexOf('i'));
+    
+    //2 search, includes,  startWith
+    console.log(str.search('i')); //1
+    console.log(str.includes('u')); //false
+    console.log(str.includes('i',4)); // if it includes from 4 position , it is false
+    console.log(str.startsWith('H')); //true
+    console.log(str.endsWith('H')); //false
