6be05109bf4afa35bb6008f4a3f58089ca6b2273
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-01-17 21:55:41 +0500

    operators

---
+++ lesson_6/index.js
@@ -0,0 +1,60 @@
+//Operators
+/*
+    * Arithmetic Operators - +; -;*;/
+    * Assignment Operators - =;  +=; -=; *=; /=;
+    * Comparison Operators - ==; ===; !=; <=; >=; >;<;
+    * Logic Operator &&;||;
+    * Ternary Operators ? :;
+*/
+
+// 1 Arithmetic operators 
+
+let a=5;
+let b=6;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//2 Assigment operators
+let x=5;
+let y=20;
+console.log(x+=y); //x=x+y 25
+console.log(x-=y); //x=x-y 5
+console.log(x*=y); //x=x-y 100
+console.log(x/=y); //x=x-y 5
+console.log(y%=x); // % qoldiqli bo'lish   0
+
+
+//3  Comparison Operators
+
+let c=10;
+let d='10';
+console.log(c==d); // true
+console.log(c===d); // strick equal fasle
+console.log(c!=d); // not equal true
+console.log(c<=d); // true
+console.log(c>=d); // true
+console.log(c>d); // false
+console.log(c<d); // false
+
+//4 logic operator & ||
+
+// && (and)
+
+console.log(true&&true); // true
+console.log(true&&false); // false
+console.log(false&&true); // false
+console.log(false&&false); // false
+
+// || (or)
+
+console.log((true||true)); //true
+console.log((true||false)); // true
+console.log((false||true)); // true
+console.log((false||false)); // false
+
+// != not equal
+
+console.log(!true); //false
+console.log(!false); //true
